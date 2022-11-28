ee570015aeb2631d78fb1c4d396c7411bdb15be8
Ilhombek <ubaydullaevilhombek681@gmail.com>
2022-11-29 01:47:41 +0500

    Initial commit

--- src/main/kotlin/com/example/hrmanagement/Documentations.kt
+++ src/main/kotlin/com/example/hrmanagement/Documentations.kt
@@ -5,6 +5,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 @MustBeDocumented
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER,
     AnnotationTarget.VALUE_PARAMETER
+
 )
 @Retention(AnnotationRetention.RUNTIME)
 @AuthenticationPrincipal
