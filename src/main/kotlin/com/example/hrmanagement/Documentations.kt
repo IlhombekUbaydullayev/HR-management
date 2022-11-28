8e19990712b4d45c981d7151c10c8da7765c1bb6
Ilhombek <ubaydullaevilhombek681@gmail.com>
2022-11-29 01:49:25 +0500

    initial

--- src/main/kotlin/com/example/hrmanagement/Documentations.kt
+++ src/main/kotlin/com/example/hrmanagement/Documentations.kt
@@ -5,7 +5,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 @MustBeDocumented
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER,
     AnnotationTarget.VALUE_PARAMETER
-
 )
 @Retention(AnnotationRetention.RUNTIME)
 @AuthenticationPrincipal
